d90c9a840daf182a5a58b973c6aea0829bc27299
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-10-29 21:37:54 -0700

    Implement the counters for the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER_NAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
 end
