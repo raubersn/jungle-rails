cc3ab63a7b9510786b1d1cab72bae4df6003457d
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-11-05 17:39:26 -0800

    Create the Sessions controller and the Login form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'about#index'
 
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
     
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
